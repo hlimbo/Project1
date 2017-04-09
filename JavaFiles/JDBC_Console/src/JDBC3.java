227bf5e33476a5d53e4e8563c9f27d98f386266d
hlimbo <limboh27@gmail.com>
2017-04-09 11:38:06 -0700

    Add JDBC Driver to JavaFiles folder

--- JavaFiles/JDBC_Console/src/JDBC3.java
+++ JavaFiles/JDBC_Console/src/JDBC3.java
@@ -2,33 +2,33 @@
 // Coded by Chen Li/Kirill Petrov Winter, 2005
 // Slightly revised for ICS185 Spring 2005, by Norman Jacobson
 
-//import java.sql.*;                              // Enable SQL processing
-//	
-// public class JDBC3
-//{
-//    public static void main(String[] arg) throws Exception
-//    {
-//        // Incorporate MySQL driver
-//		 Class.forName("com.mysql.jdbc.Driver").newInstance();
-//		   
-//		            // Connect to the test database
-//		Connection connection =
-//		DriverManager.getConnection("jdbc:mysql:///moviedb?autoReconnect=true&useSSL=false", "mytestuser", "mypassword");
-//		   
-//		// prepare SQL statement template that's to be repeatedly excuted
-//		String updateString = "update stars set first_name = ? where id = ?";
-//		            PreparedStatement updateStars = connection.prepareStatement(updateString);
-//		   
-//        // values for first and second "?" wildcard in statement template
-//		int [] ids = {755011, 755017};
-//		String [] firstNames = {"New Arnold", "New Eddie"};
-//		   
-//        // for each record in table, update to new values given
-//		for(int i = 0; i < ids.length; i++)
-//	    {
-//	        updateStars.setString(1, firstNames[i]);
-//	        updateStars.setInt(2, ids[i]);
-//	        updateStars.executeUpdate();
-//	    }
-//    }
-//}
+import java.sql.*;                              // Enable SQL processing
+	
+ public class JDBC3
+{
+    public static void main(String[] arg) throws Exception
+    {
+        // Incorporate MySQL driver
+		 Class.forName("com.mysql.jdbc.Driver").newInstance();
+		   
+		            // Connect to the test database
+		Connection connection =
+		DriverManager.getConnection("jdbc:mysql:///moviedb?autoReconnect=true&useSSL=false", "mytestuser", "mypassword");
+		   
+		// prepare SQL statement template that's to be repeatedly excuted
+		String updateString = "update stars set first_name = ? where id = ?";
+		            PreparedStatement updateStars = connection.prepareStatement(updateString);
+		   
+        // values for first and second "?" wildcard in statement template
+		int [] ids = {755011, 755017};
+		String [] firstNames = {"New Arnold", "New Eddie"};
+		   
+        // for each record in table, update to new values given
+		for(int i = 0; i < ids.length; i++)
+	    {
+	        updateStars.setString(1, firstNames[i]);
+	        updateStars.setInt(2, ids[i]);
+	        updateStars.executeUpdate();
+	    }
+    }
+}
