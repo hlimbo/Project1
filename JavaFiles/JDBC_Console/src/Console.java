e9ba5648d22225c140b3b805746e731971d14f0e
hlimbo <limboh27@gmail.com>
2017-04-10 13:55:39 -0700

    ..

--- JavaFiles/JDBC_Console/src/Console.java
+++ JavaFiles/JDBC_Console/src/Console.java
@@ -16,10 +16,9 @@ public class Console {
 		System.out.println("8. Exit the program");
 	}
 	
-	private static void printMoviesGivenFeaturedStar(Connection connection) throws Exception
+	//todo input validation
+	private static void printMoviesGivenFeaturedStar(Connection connection,Scanner scan) throws Exception
 	{
-		Scanner scan = new Scanner(System.in);
-		
 		System.out.print("Enter the first and last name or id of the star to display the movies they are featured in: ");
 		String response = scan.nextLine();
 		Integer star_id = -1;
@@ -67,15 +66,13 @@ public class Console {
 			System.out.print(result.getString(5) + " \t");
 			System.out.println(result.getString(6) + " \t");
 		}
-		
-		scan.close();
 	}
 	
-	private static void insertNewStar(Connection connection) throws Exception
+	private static void insertNewStar(Connection connection,Scanner scan) throws Exception
 	{
-		Scanner scanner = new Scanner(System.in);
+		
 		System.out.print("Enter new star name to insert into database: ");
-		String response = scanner.nextLine();
+		String response = scan.nextLine();
 		String[] name = response.split(" ");
 		String first_name = name[0];
 		String last_name = name[1];
@@ -83,20 +80,63 @@ public class Console {
 		Statement update = connection.createStatement();
 		int retID = update.executeUpdate(insertString);
 		System.out.println("retID = " + retID);
-		scanner.close();
 	}
 	
-	private static void insertNewCustomer(Connection connection) throws Exception
+	private static void insertNewCustomer(Connection connection, Scanner scan) throws Exception
+	{
+		System.out.print("Enter new customer full name: ");
+		String response = scan.nextLine();
+		
+		String[] name = response.split(" ");
+		String first_name = null;
+		String last_name = null;
+		
+		if(name.length == 2)
+		{
+			first_name = name[0];
+			last_name = name[1];
+		}
+		else if(name.length == 1)
+		{
+			last_name = name[0];
+			first_name = "";
+		}
+		
+		///Find creditcard holder's id given creditcard holder's first name and last name.
+		//Select id from creditcards where first_name="first" and last_name="last
+		String ccQuery = "SELECT id FROM creditcards WHERE first_name = ? and last_name= ?";
+		PreparedStatement ccStatement = connection.prepareStatement(ccQuery);
+		ccStatement.setString(1, first_name);
+		ccStatement.setString(2, last_name);
+		ResultSet resultIDSet = ccStatement.executeQuery();
+		
+		//if no credit card ids are associated with the first and last name provided.
+		if(!resultIDSet.next())
+		{
+			System.out.println("Cannot add " + first_name + " " + last_name + " into the database ");
+			return;
+		}
+		
+		//insert the customer into the database
+		String insertSQL = "INSERT INTO customers (first_name, last_name, address, email, password) VALUES(?, ?, ?, ?, ?)";	
+		PreparedStatement insertStatement = connection.prepareStatement(insertSQL);
+		insertStatement.setString(1, first_name);
+		insertStatement.setString(2, last_name);
+		//todo: have user probably insert address, email, and password.
+		insertStatement.setString(3, "123 fake street");
+		insertStatement.setString(4, "fake@email.net");
+		insertStatement.setString(5, "patheticpassword");		
+		int retID = insertStatement.executeUpdate();
+		
+		System.out.println("Successfully inserted: " + first_name + " " + last_name + " into the customers table");
+	}
+	
+	private static void deleteCustomer(Connection connection, Scanner scan) throws Exception
 	{
 		
 	}
 	
-	private static void deleteCustomer(Connection connection) throws Exception
-	{
-		
-	}
-	
-	private static void displayMetadata(Connection connection) throws Exception
+	private static void displayMetadata(Connection connection, Scanner scan) throws Exception
 	{				
 		DatabaseMetaData metadataDB = connection.getMetaData();
 		ResultSet tables = metadataDB.getTables(connection.getCatalog(), null, "%", null);
@@ -120,20 +160,33 @@ public class Console {
 		
 	}
 	
-	private static void enterSQLCommand(Connection connection) throws Exception
+	private static void enterSQLCommand(Connection connection, Scanner scan) throws Exception
 	{
 		
 	}
 	
 	//needs to logout user from the database..
-	private static boolean exitMenu(Connection connection) throws Exception
+	private static boolean exitMenu(Connection connection, Scanner scan) throws Exception
 	{
 		return true;
 	}
 	
-	private static boolean exitProgram(Connection connection) throws Exception
+	private static boolean exitProgram(Connection connection, Scanner scan) throws Exception
 	{
 		return true;
+	}
+	
+	private static int parseChoice(Scanner scan)
+	{
+		while(scan.hasNext())
+		{
+			if(scan.hasNextInt())
+				return scan.nextInt();
+			else
+				scan.next();
+		}
+		
+		return -1;
 	}
 	
 	public static void main(String[] args) throws Exception
@@ -143,41 +196,43 @@ public class Console {
 		
 		Connection connection = DriverManager.getConnection("jdbc:mysql:///moviedb?autoReconnect=true&useSSL=false","root", "password");
 		
+		
+		//todo scanning for input breaks after entering string as input.
 		Scanner scan = new Scanner(System.in);
 		
 		boolean done = false;
-		
 		
 		while(!done)
 		{
 			displayMenuOptions();
 			System.out.print("Enter numeric choice (1-8): ");
-			Integer choice = scan.nextInt();
+			Integer choice = parseChoice(scan);
+			scan.nextLine();
 			switch(choice)
 			{
 			case 1:
-				printMoviesGivenFeaturedStar(connection);
+				printMoviesGivenFeaturedStar(connection,scan);
 				break;
 			case 2:
-				insertNewStar(connection);
+				insertNewStar(connection, scan);
 				break;
 			case 3:
-				insertNewCustomer(connection);
+				insertNewCustomer(connection,scan);
 				break;
 			case 4:
-				deleteCustomer(connection);
+				deleteCustomer(connection,scan);
 				break;
 			case 5:
-				displayMetadata(connection);
+				displayMetadata(connection,scan);
 				break;
 			case 6:
-				enterSQLCommand(connection);
+				enterSQLCommand(connection,scan);
 				break;
 			case 7:
-				done = exitMenu(connection);
+				done = exitMenu(connection,scan);
 				break;
 			case 8:
-				done = exitProgram(connection);
+				done = exitProgram(connection,scan);
 				break;
 			default:
 				System.out.println("Invalid choice... program will now exit");
